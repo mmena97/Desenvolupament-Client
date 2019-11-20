fada62b1ae39d9f9e07ad241202d718f38558490
Miguel Angel <mena_alcalde@hotmail.com>
2019-11-20 12:49:22 +0100

    cambio nombres

---  Datatables/offer.php
+++  Datatables/offer.php
@@ -1,6 +1,6 @@
 <?php
   header("Content-Type: application/json; charset=UTF-8");
-  $conn = new mysqli("server", "mena", "123","datatables");
+  $conn = new mysqli("localhost", "mena", "123","datatables");
   $stmt = $conn->prepare("SELECT * FROM offer");
   $stmt->execute();
   $result = $stmt->get_result();
